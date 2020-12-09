1a1bfa701bded8e1135e349ad4c5d15088ae06e7
AasthaGithub <aasthamehta2704@gmail.com>
2020-12-09 09:44:13 +0530

    Policy Services Updated

---
+++ Vehicle_Insurance_SpringBoot/src/main/java/com/lti/project/bean/Policy.java
@@ -0,0 +1,126 @@
+package com.lti.project.bean;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="policies")
+public class Policy {
+	
+	@Id
+	@Column(name="Policy_Num")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="POLICY_GEN")
+    //@SequenceGenerator(name="POLICY_GEN",sequenceName="policy_seq",allocationSize=1)
+	private int policyNum;
+	
+	@Column(name="Vehicle_Reg_Num")
+	private String vehicleRegNum;
+	
+	@Column(name="User_Id")
+	private int userId;
+	
+	@Column(name="Plan_Id")
+	private int planId;
+	
+	@Column(name="Period")
+	private int period;
+	
+	@Column(name="Start_Date")
+	private Date startDate;
+	
+	@Column(name="End_Date")
+	private Date endDate;
+
+	public Policy() {
+		super();
+	}
+
+	public Policy(int policyNum, String vehicleRegNum, int userId, int planId, int period, Date startDate,
+			Date endDate) {
+		super();
+		this.policyNum = policyNum;
+		this.vehicleRegNum = vehicleRegNum;
+		this.userId = userId;
+		this.planId = planId;
+		this.period = period;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}	
+	
+	@Override
+	public String toString() {
+		return "Policy [policyNum=" + policyNum + ", vehicleRegNum=" + vehicleRegNum + ", userId=" + userId
+				+ ", planId=" + planId + ", period=" + period + ", startDate=" + startDate + ", endDate=" + endDate
+				+ "]";
+	}
+
+	public int getPolicyNum() {
+		return policyNum;
+	}
+
+	public void setPolicyNum(int policyNum) {
+		this.policyNum = policyNum;
+	}
+
+	public String getVehicleRegNum() {
+		return vehicleRegNum;
+	}
+
+	public void setVehicleRegNum(String vehicleRegNum) {
+		this.vehicleRegNum = vehicleRegNum;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getPlanId() {
+		return planId;
+	}
+
+	public void setPlanId(int planId) {
+		this.planId = planId;
+	}
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
+	
+	
+	
+
+}
--- Vehicle_Insurance_SpringBoot/src/main/java/com/lti/project/service/PolicyServiceImpl.java
+++ Vehicle_Insurance_SpringBoot/src/main/java/com/lti/project/service/PolicyServiceImpl.java
@@ -1,5 +1,42 @@
 package com.lti.project.service;
 
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lti.project.bean.Policy;
+import com.lti.project.dao.PolicyDao;
+import com.lti.project.exceptions.HrExceptions;
+
 public class PolicyServiceImpl implements PolicyService {
+
+	@Autowired
+	private PolicyDao policy_dao;
+	
+	@Override
+	public List<Policy> getAllPolicies() throws HrExceptions {
+		return policy_dao.getAllPolicies();
+	}
+
+	@Override
+	public boolean addPolicy(Policy p) throws HrExceptions {
+		return policy_dao.addPolicy(p);
+	}
+
+	@Override
+	public int updatePolicyEndDate(int id, Date newEndDate) throws HrExceptions {
+		return policy_dao.updatePolicyEndDate(id, newEndDate);
+	}
+
+	@Override
+	public boolean deletePolicy(int id) throws HrExceptions {
+		return policy_dao.deletePolicy(id);
+	}
+
+	@Override
+	public Policy findPolicyById(int id) throws HrExceptions {
+		return policy_dao.findPolicyById(id);
+	}
 
 }
